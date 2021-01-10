769f95c8680e61c0e394358f1c9bec685730f19c
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-01-11 04:08:36 +0800

    move tests from src to test folder

--- test/graph-sort.spec.ts
+++ test/graph-sort.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 import { SinonSpy, spy } from 'sinon'
-import { CompareFn, GraphSort } from './graph-sort'
+import { CompareFn, GraphSort } from '../src/graph-sort'
 
 describe('graph-sort TestSuit', () => {
   let compareFn: CompareFn<number> = (a, b) =>
@@ -69,5 +69,25 @@ describe('graph-sort TestSuit', () => {
     test(values)
   })
 
-  it('should sort', function () {})
+  it('should not has error when the compareFn is not consistent', function () {
+    let N = 2000
+    let values: number[] = []
+    for (let i = 0; i < N; i++) {
+      values.push(Math.random() * 1000)
+    }
+    let compareFn: CompareFn<number> = (a, b) => {
+      return Math.random() < 0.5
+        ? { small: a, large: b }
+        : { large: a, small: b }
+    }
+    compareFnSpy = spy(compareFn)
+    graph = new GraphSort(compareFnSpy)
+    graph.addValues(values)
+    let actualTopNValues = graph.popTopN(N)
+    expect(actualTopNValues).length(N)
+    expect(actualTopNValues).not.deep.equals(values)
+    expect(new Set(actualTopNValues)).deep.equals(new Set(values))
+    expect(compareFnSpy.callCount).greaterThan(N - 2)
+    expect(compareFnSpy.callCount).lessThan(N * (N - 1))
+  })
 })
